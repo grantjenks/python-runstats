ff67606049e1473eb8659aa5eba22c2c928687cb
Grant Jenks <contact@grantjenks.com>
2016-11-03 10:04:50 -0700

    Bump version to 0.6.0

--- runstats.py
+++ runstats.py
@@ -255,8 +255,8 @@ class Regression(object):
         return self
 
 __title__ = 'runstats'
-__version__ = '0.5.4'
-__build__ = 0x000504
+__version__ = '0.6.0'
+__build__ = 0x000600
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2015 Grant Jenks'
