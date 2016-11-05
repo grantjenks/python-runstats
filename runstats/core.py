e625eb6c437e86729b918744ce102f6c3a306933
Grant Jenks <contact@grantjenks.com>
2016-11-05 14:26:36 -0700

    Move runstats module to core

--- runstats/core.py
+++ runstats/core.py
@@ -253,10 +253,3 @@ class Regression(object):
         self._sxy = sum_sxy
 
         return self
-
-__title__ = 'runstats'
-__version__ = '0.6.0'
-__build__ = 0x000600
-__author__ = 'Grant Jenks'
-__license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2015 Grant Jenks'
