2f803b74d46b01866f694c1836ffa1b77ab068b5
Grant Jenks <contact@grantjenks.com>
2016-11-05 14:29:10 -0700

    Add simple benchmark

---
+++ tests/benchmark.py
@@ -0,0 +1,84 @@
+"""Benchmark core versus fast implementations.
+
+"""
+
+from __future__ import print_function
+
+import random
+import timeit
+
+random.seed(0)
+values = [random.random() for _ in range(int(1e4))]
+pairs = [(pos, pos + (val * 2 - 1)) for pos, val in enumerate(values)]
+
+core_stats = timeit.repeat(
+    setup="""
+from __main__ import values
+from runstats.core import Statistics
+stats = Statistics()
+    """,
+    stmt="""
+for value in values:
+    stats.push(value)
+stats.mean()
+    """,
+    number=1,
+    repeat=5,
+)[2]
+
+fast_stats = timeit.repeat(
+    setup="""
+from __main__ import values
+from runstats.fast import Statistics
+stats = Statistics()
+    """,
+    stmt="""
+for value in values:
+    stats.push(value)
+stats.mean()
+    """,
+    number=1,
+    repeat=5,
+)[2]
+
+speedup_stats = (core_stats - fast_stats) / core_stats * 100
+
+core_regr = timeit.repeat(
+    setup="""
+from __main__ import pairs
+from runstats.core import Regression
+regr = Regression()
+    """,
+    stmt="""
+for pos, val in pairs:
+    regr.push(pos, val)
+regr.slope()
+    """,
+    number=1,
+    repeat=5,
+)[2]
+
+fast_regr = timeit.repeat(
+    setup="""
+from __main__ import pairs
+from runstats.fast import Regression
+regr = Regression()
+    """,
+    stmt="""
+for pos, val in pairs:
+    regr.push(pos, val)
+regr.slope()
+    """,
+    number=1,
+    repeat=5,
+)[2]
+
+speedup_regr = (core_regr - fast_regr) / core_regr * 100
+
+print('core.Statistics:', core_stats)
+print('fast.Statistics:', fast_stats)
+print('Stats Speedup:  ', '%.2f%%' % speedup_stats)
+
+print('core.Regression:', core_regr)
+print('fast.Regression:', fast_regr)
+print('Regr Speedup:   ', '%.2f%%' % speedup_regr)
