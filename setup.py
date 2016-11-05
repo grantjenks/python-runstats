70c8493e3aaab22dd3de78e5208e1b6d82540103
Grant Jenks <contact@grantjenks.com>
2016-11-05 14:31:10 -0700

    Add Cython extension to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-
-import sys
-from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
+from Cython.Build import cythonize
 import runstats
+from setuptools import Extension, find_packages, setup
+from setuptools.command.test import test as TestCommand
+import sys
+
 
 class Tox(TestCommand):
     def finalize_options(self):
@@ -16,38 +15,39 @@ class Tox(TestCommand):
         errno = tox.cmdline(self.test_args)
         sys.exit(errno)
 
-with open('README.rst') as f:
-    readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
+with open('README.rst') as reader:
+    readme = reader.read()
 
 setup(
-    name='runstats',
+    name=runstats.__title__,
     version=runstats.__version__,
     description='Compute statistics and regression in one pass',
     long_description=readme,
     author='Grant Jenks',
     author_email='contact@grantjenks.com',
     url='http://www.grantjenks.com/docs/runstats/',
-    py_modules=['runstats'],
-    package_data={'': ['LICENSE', 'README.rst']},
+    license='Apache 2.0',
+    packages=find_packages(exclude=('tests', 'docs')),
+    ext_modules=cythonize([Extension('runstats.fast', ['runstats/fast.pyx'])]),
     tests_require=['tox'],
     cmdclass={'test': Tox},
-    license=license,
+    install_requires=[],
     classifiers=(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ),
 )
