cdcc84b2c658ea337589fd573e03c7bf6c19489a
Grant Jenks <contact@grantjenks.com>
2016-11-05 20:12:51 -0700

    Bump version to 1.0.0

--- runstats/__init__.py
+++ runstats/__init__.py
@@ -8,8 +8,8 @@ except ImportError:
     from .core import Statistics, Regression
 
 __title__ = 'runstats'
-__version__ = '0.6.0'
-__build__ = 0x000600
+__version__ = '1.0.0'
+__build__ = 0x010000
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2015 Grant Jenks'
