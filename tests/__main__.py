3e61d3e4d381c083601d08115bdd3b11f362d99d
Grant Jenks <contact@grantjenks.com>
2019-02-12 11:23:19 -0800

    Add __name__ check for import by pytest

--- tests/__main__.py
+++ tests/__main__.py
@@ -9,65 +9,68 @@ from runstats.core import Regression as CoreRegression
 
 from .test_runstats import mean, variance, stddev, skewness, kurtosis
 
+def main():
+    args = list(map(float, sys.argv[1:]))
 
-args = list(map(float, sys.argv[1:]))
+    print('Statistics Functions')
+    print('Count:', len(args))
+    print('Mean:', mean(args))
+    print('Variance:', variance(args))
+    print('StdDev:', stddev(args))
+    print('Skewness:', skewness(args))
+    print('Kurtosis:', kurtosis(args))
 
-print('Statistics Functions')
-print('Count:', len(args))
-print('Mean:', mean(args))
-print('Variance:', variance(args))
-print('StdDev:', stddev(args))
-print('Skewness:', skewness(args))
-print('Kurtosis:', kurtosis(args))
+    fast_stats = FastStatistics()
 
-fast_stats = FastStatistics()
+    for arg in args:
+        fast_stats.push(arg)
 
-for arg in args:
-    fast_stats.push(arg)
+    print()
+    print('FastStatistics')
+    print('Count:', len(fast_stats))
+    print('Mean:', fast_stats.mean())
+    print('Variance:', fast_stats.variance())
+    print('StdDev:', fast_stats.stddev())
+    print('Skewness:', fast_stats.skewness())
+    print('Kurtosis:', fast_stats.kurtosis())
 
-print()
-print('FastStatistics')
-print('Count:', len(fast_stats))
-print('Mean:', fast_stats.mean())
-print('Variance:', fast_stats.variance())
-print('StdDev:', fast_stats.stddev())
-print('Skewness:', fast_stats.skewness())
-print('Kurtosis:', fast_stats.kurtosis())
+    core_stats = CoreStatistics()
 
-core_stats = CoreStatistics()
+    for arg in args:
+        core_stats.push(arg)
 
-for arg in args:
-    core_stats.push(arg)
+    print()
+    print('CoreStatistics')
+    print('Count:', len(core_stats))
+    print('Mean:', core_stats.mean())
+    print('Variance:', core_stats.variance())
+    print('StdDev:', core_stats.stddev())
+    print('Skewness:', core_stats.skewness())
+    print('Kurtosis:', core_stats.kurtosis())
 
-print()
-print('CoreStatistics')
-print('Count:', len(core_stats))
-print('Mean:', core_stats.mean())
-print('Variance:', core_stats.variance())
-print('StdDev:', core_stats.stddev())
-print('Skewness:', core_stats.skewness())
-print('Kurtosis:', core_stats.kurtosis())
+    fast_regr = FastRegression()
 
-fast_regr = FastRegression()
+    for index, arg in enumerate(args, 1):
+        fast_regr.push(index, arg)
 
-for index, arg in enumerate(args, 1):
-    fast_regr.push(index, arg)
+    print()
+    print('FastRegression')
+    print('Count:', len(fast_regr))
+    print('Slope:', fast_regr.slope())
+    print('Intercept:', fast_regr.intercept())
+    print('Correlation:', fast_regr.correlation())
 
-print()
-print('FastRegression')
-print('Count:', len(fast_regr))
-print('Slope:', fast_regr.slope())
-print('Intercept:', fast_regr.intercept())
-print('Correlation:', fast_regr.correlation())
+    core_regr = CoreRegression()
 
-core_regr = CoreRegression()
+    for index, arg in enumerate(args, 1):
+        core_regr.push(index, arg)
 
-for index, arg in enumerate(args, 1):
-    core_regr.push(index, arg)
+    print()
+    print('CoreRegression')
+    print('Count:', len(core_regr))
+    print('Slope:', core_regr.slope())
+    print('Intercept:', core_regr.intercept())
+    print('Correlation:', core_regr.correlation())
 
-print()
-print('CoreRegression')
-print('Count:', len(core_regr))
-print('Slope:', core_regr.slope())
-print('Intercept:', core_regr.intercept())
-print('Correlation:', core_regr.correlation())
+if __name__ == '__main__':
+    main()
--- tests/benchmark.py
+++ tests/benchmark.py
@@ -7,78 +7,82 @@ from __future__ import print_function
 import random
 import timeit
 
-random.seed(0)
-values = [random.random() for _ in range(int(1e4))]
-pairs = [(pos, pos + (val * 2 - 1)) for pos, val in enumerate(values)]
+def main():
+    random.seed(0)
+    values = [random.random() for _ in range(int(1e4))]
+    pairs = [(pos, pos + (val * 2 - 1)) for pos, val in enumerate(values)]
 
-core_stats = timeit.repeat(
-    setup="""
-from __main__ import values
-from runstats.core import Statistics
-stats = Statistics()
-    """,
-    stmt="""
-for value in values:
-    stats.push(value)
-stats.mean()
-    """,
-    number=1,
-    repeat=7,
-)[2]
+    core_stats = timeit.repeat(
+        setup="""
+    from __main__ import values
+    from runstats.core import Statistics
+    stats = Statistics()
+        """,
+        stmt="""
+    for value in values:
+        stats.push(value)
+    stats.mean()
+        """,
+        number=1,
+        repeat=7,
+    )[2]
 
-fast_stats = timeit.repeat(
-    setup="""
-from __main__ import values
-from runstats.fast import Statistics
-stats = Statistics()
-    """,
-    stmt="""
-for value in values:
-    stats.push(value)
-stats.mean()
-    """,
-    number=1,
-    repeat=7,
-)[2]
+    fast_stats = timeit.repeat(
+        setup="""
+    from __main__ import values
+    from runstats.fast import Statistics
+    stats = Statistics()
+        """,
+        stmt="""
+    for value in values:
+        stats.push(value)
+    stats.mean()
+        """,
+        number=1,
+        repeat=7,
+    )[2]
 
-speedup_stats = core_stats / fast_stats - 1
+    speedup_stats = core_stats / fast_stats - 1
 
-core_regr = timeit.repeat(
-    setup="""
-from __main__ import pairs
-from runstats.core import Regression
-regr = Regression()
-    """,
-    stmt="""
-for pos, val in pairs:
-    regr.push(pos, val)
-regr.slope()
-    """,
-    number=1,
-    repeat=7,
-)[2]
+    core_regr = timeit.repeat(
+        setup="""
+    from __main__ import pairs
+    from runstats.core import Regression
+    regr = Regression()
+        """,
+        stmt="""
+    for pos, val in pairs:
+        regr.push(pos, val)
+    regr.slope()
+        """,
+        number=1,
+        repeat=7,
+    )[2]
 
-fast_regr = timeit.repeat(
-    setup="""
-from __main__ import pairs
-from runstats.fast import Regression
-regr = Regression()
-    """,
-    stmt="""
-for pos, val in pairs:
-    regr.push(pos, val)
-regr.slope()
-    """,
-    number=1,
-    repeat=7,
-)[2]
+    fast_regr = timeit.repeat(
+        setup="""
+    from __main__ import pairs
+    from runstats.fast import Regression
+    regr = Regression()
+        """,
+        stmt="""
+    for pos, val in pairs:
+        regr.push(pos, val)
+    regr.slope()
+        """,
+        number=1,
+        repeat=7,
+    )[2]
 
-speedup_regr = core_regr / fast_regr - 1
+    speedup_regr = core_regr / fast_regr - 1
 
-print('core.Statistics:', core_stats)
-print('fast.Statistics:', fast_stats)
-print('  Stats Speedup: %.2fx faster' % speedup_stats)
+    print('core.Statistics:', core_stats)
+    print('fast.Statistics:', fast_stats)
+    print('  Stats Speedup: %.2fx faster' % speedup_stats)
 
-print('core.Regression:', core_regr)
-print('fast.Regression:', fast_regr)
-print('   Regr Speedup: %.2fx faster' % speedup_regr)
+    print('core.Regression:', core_regr)
+    print('fast.Regression:', fast_regr)
+    print('   Regr Speedup: %.2fx faster' % speedup_regr)
+
+if __name__ == '__main__':
+    main()
