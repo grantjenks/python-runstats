1012f943721e78d3186f1d971e021bf92bf45707
Grant Jenks <contact@grantjenks.com>
2019-02-12 11:46:25 -0800

    Bump version to 1.8.0

--- runstats/__init__.py
+++ runstats/__init__.py
@@ -12,8 +12,8 @@ except ImportError:
     __compiled__ = False
 
 __title__ = 'runstats'
-__version__ = '1.7.1'
-__build__ = 0x010701
+__version__ = '1.8.0'
+__build__ = 0x010800
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2015-2017 Grant Jenks'
+__copyright__ = '2013-2019, Grant Jenks'
