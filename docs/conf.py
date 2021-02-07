aabca629be575f45f170d59720ddd1d6f3994850
Alan Mazankiewicz <alan.mazankiewicz@live.de>
2021-02-07 17:41:53 +0100

    corrected for flake8

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,9 @@
 
 import os
 import sys
+import runstats
 
 sys.path.insert(0, os.path.abspath('..'))
-import runstats
 
 
 # -- Project information -----------------------------------------------------
